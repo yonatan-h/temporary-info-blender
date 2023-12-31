55f418a1f163c100ecc697eefb3da41c13a236cd
Yonatan <yon019hl@gmail.com>
2023-12-31 23:35:06 +0300

    clean up

--- api/index.js
+++
@@ -1,5 +0,0 @@
-import express from 'express'
-const app = express()
-console.log('here')
-app.get('/', (req, res) => res.send('Hello World!'))
-export default app
--- api/start.js
+++
@@ -1,2 +0,0 @@
-import app from "./index.js";
-app.listen(3000, () => console.log("Example app listening on port 3000!"));
